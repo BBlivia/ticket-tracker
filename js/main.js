15e5e8b8ae177e235527d14d5860b66e87fd89cc
Olivia Bloway <blowayol@msu.edu>
2022-04-03 16:20:30 -0400

    added function to save and fetch issues

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue)
+
+
+
+function fetchIssues(){
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    let issuesList = document.getElementById('issuesList')
+
+    issuesList.innerHTML = '';
+
+    for(let i = 0; i < issues.length; i++){
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == 'Closed'? 'label-success' : 'label-info'
+
+        //issuesList.innerHTML += 
+    }
+}
+
+
+function saveIssue(e){
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput')
+    let issueDescription = document.getElementById('issueDesInput')
+    let issueSeverity = document.getElementById('issueSeverityInput')
+    let issueAssignedTo = document.getElementById('issueAssignedInput')
+}
+

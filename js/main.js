292484330e322128d681379a46461289e9560c17
Olivia Bloway <blowayol@msu.edu>
2022-04-04 16:31:58 -0400

    added form and sumbit event function

--- js/main.js
+++ js/main.js
@@ -28,5 +28,35 @@ function saveIssue(e){
     let issueDescription = document.getElementById('issueDesInput')
     let issueSeverity = document.getElementById('issueSeverityInput')
     let issueAssignedTo = document.getElementById('issueAssignedInput')
+    let issueStatus = 'Open'
+
+
+    // indivisual issue 
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDescription,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    // checking if item is in storage, so it will be stored and saved in local storage 
+    if( localStorage.getItem('issues' === null)){
+       let issues = []
+       issues.push(issue)
+       localStorage.setItem('issues', JSON.stringify(issues))
+    }else{
+        let issues = JSON.parse(localStorage.getItem('issues'))
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
+
+    document.getElementById('issueInputForm').reset()
+
+
+    fetchIssues()
+
+    e.preventDefault()
 }
 
